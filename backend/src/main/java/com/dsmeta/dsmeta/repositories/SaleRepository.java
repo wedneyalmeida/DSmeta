97d35400f1cd050f025719de4992259dd3ce0714
Wedney Almeida <wedneyalmeida.dev@gmail.com>
2022-11-17 08:04:00 -0300

    API test

---
+++ backend/src/main/java/com/dsmeta/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.dsmeta.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dsmeta.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
